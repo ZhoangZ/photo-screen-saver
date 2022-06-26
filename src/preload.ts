8fbcb8451b5785b9aa8a91b5d0d64c5a7ef188a5
Hoang Le Tan <tanhoang.le@mafc.com.vn>
2022-06-27 01:02:53 +0700

    Add recursively

--- src/preload.ts
+++ src/preload.ts
@@ -1,25 +1,37 @@
-import fs from "fs"
 import { contextBridge } from "electron"
 import { LOCAL_FOLDER_PATH } from "./constants"
 import { Photo } from "./photo"
+import fs from "fs"
+import path from "path"
 
 contextBridge.exposeInMainWorld("api", { getLocalPhotos })
 
 function getLocalPhotos(): Photo[]
 {
+   
    let folderPath = LOCAL_FOLDER_PATH
-   if(!folderPath.endsWith("/"))
-      folderPath += "/"
+   let fileNames:String[]=[]
+   function traverseDir(dir:String) {   
+      fs.readdirSync(dir.toString()).forEach(file => {
+        let fullPath = path.join(dir.toString(), file)
+        if (fs.lstatSync(fullPath).isDirectory()) {
+           traverseDir(fullPath)
+         } else {
+            fileNames.push(fullPath)
+         }  
+      })
+   }
+   traverseDir(folderPath)
 
-   const fileNames = fs.readdirSync(folderPath)
-
-   const photos = fileNames
+   const photos = fileNames.slice()
       .filter(fn => fn.match(/\.(jpg|jpeg)$/i) != null)
       .map(fn => ({
-         url: `file:///${folderPath}${fn}`,
+         url: `file:///${fn.replaceAll("\\", "/")}`,
          title: "",
          attribution: "",
       }))
 
    return photos
 }
+
+
